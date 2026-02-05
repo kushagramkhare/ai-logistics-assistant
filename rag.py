63d2fa14b3d7171105a03a840f1f4c249bcb2e78
nih8 <b24cs1045@iitj.ac.in>
2026-02-06 03:22:21 +0530

    memory added
    added memory for contextualization

--- rag.py
+++ rag.py
@@ -6,6 +6,8 @@ from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.tools import tool
 from langchain.agents import create_agent
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import HumanMessage, AIMessage
 
 # 1. ENV SETUP
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1" 
@@ -50,23 +52,27 @@ def logistics_search(query: str) -> str:
 
 # 5. AGENT SETUP
 tools = [logistics_search]
+chat_history = []
+
 agent = create_agent(
     model=model,
     tools=tools,
-    system_prompt=(
-        "You are a helpful assistant. Always use the logistics_search tool to find facts. "
-        "If the tool says information is missing or irrelevant, inform the user "
-        "honestly instead of guessing."
-    )
+    system_prompt="You are a helpful assistant. Always use the logistics_search tool to find facts.If the tool says information is missing or irrelevant, inform the user honestly instead of guessing.",
 )
 
 # 6. RUN
 if __name__ == "__main__":
     try:
         print("🚀 System Online...")
-        user_input = "When is Salman's birthday?"
-        res = agent.invoke({"messages": [{"role": "user", "content": user_input}]})
-        final_answer = res["messages"][-1]
-        print("\n✨ ANSWER:", final_answer.text)
+        while True:
+            user_query = input()
+            if user_query.lower() in ["exit", "quit"]:
+                break
+            chat_history.append(HumanMessage(content=user_query))
+            res = agent.invoke({"messages":chat_history})
+            final_answer = res["messages"][-1]
+            print("\n✨ ANSWER:", final_answer.text)
+            
+            chat_history.append(AIMessage(content=final_answer.text))
     except Exception as e:
         print(f"❌ Error: {e}")
