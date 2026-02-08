f33eb3e0f3fb29a4682bbc8d6e2157800fb7a211
nih8 <b24cs1045@iitj.ac.in>
2026-02-08 17:16:49 +0530

    update for ocr

---
+++ setup.bat
@@ -0,0 +1,9 @@
+@echo off
+pip install -r requirements.txt
+
+echo.
+echo ==============================
+echo Install Tesseract OCR (one time)
+echo https://github.com/UB-Mannheim/tesseract/wiki
+echo ==============================
+pause
