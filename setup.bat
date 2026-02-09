2267b98a9af7949e72707ceb1cec4f203176dafe
nih8 <b24cs1045@iitj.ac.in>
2026-02-09 10:26:52 +0530

    Delete setup.bat

--- setup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-pip install -r req.txt
-
-echo.
-echo ==============================
-echo Install Tesseract OCR (one time)
-echo https://github.com/UB-Mannheim/tesseract/wiki
-echo ==============================
-pause
